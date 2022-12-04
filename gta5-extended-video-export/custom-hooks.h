cf5406d23e5a0c8b1055a22c3d2e428f27b1bad0
taylorkarras <taylorkarras@gmail.com>
2022-12-04 12:55:10 -0800

    new stuff

--- gta5-extended-video-export/custom-hooks.h
+++ gta5-extended-video-export/custom-hooks.h
@@ -9,17 +9,27 @@
 //#include "..\PolyHook\PolyHook\PolyHook.hpp"
 #include "logger.h"
 
+class VFuncDetour : public PLH::x64Detour
+{
+public:
+	static std::unique_ptr<x64Detour> vDetour(void* func, uint_fast16_t vFuncIndex, void* funcHook, uint64_t tramp, PLH::ADisassembler& dis) {
+		uint64_t func_p = reinterpret_cast<uint64_t>(func);
+		uint64_t funcHook_p = (uint64_t)funcHook;
+		return std::make_unique<x64Detour>((uint64_t)(func_p+vFuncIndex), funcHook_p, &tramp, dis);
+	}
+};
+
 namespace {
 
 	auto logger = std::make_shared<PolyHookLog>();
 	uint64_t hookFuncTramp;
 	uint64_t VHookFuncTramp;
 	uint64_t oOriginalVar;
+	PLH::CapstoneDisassembler dis(PLH::Mode::x64);
 
 	template <class CLASS_TYPE>
 	HRESULT hookVirtualFunction(CLASS_TYPE *pInstance, int vFuncIndex, LPVOID hookFunc, std::shared_ptr<PLH::x64Detour> VFuncDetour_Ex) {
-		PLH::CapstoneDisassembler dis(PLH::Mode::x64);
-		VFuncDetour_Ex = std::make_unique<PLH::x64Detour>(*(char*)(pInstance+vFuncIndex), (uint64_t)hookFunc, &VHookFuncTramp, dis));
+			VFuncDetour_Ex = VFuncDetour::vDetour(pInstance, vFuncIndex, hookFunc, VHookFuncTramp, dis);
 		if (!VFuncDetour_Ex->hook()) {
 			return E_FAIL;
 		}
@@ -37,7 +47,6 @@ namespace {
 
 	template <class = void>
 	HRESULT hookX64Function(LPVOID func, LPVOID hookFunc, std::shared_ptr<PLH::x64Detour> X64Detour_ex) {
-		PLH::CapstoneDisassembler dis(PLH::Mode::x64);
 		X64Detour_ex.reset(new PLH::x64Detour((char*)func, (char*)hookFunc, &hookFuncTramp, dis));
 		if (!X64Detour_ex->hook()) {
 			return E_FAIL;
